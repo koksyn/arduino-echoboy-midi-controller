591c1bf26e0322b1a8a7b374077059092613d41b
Mateusz Palichleb <mateusz1p@gmail.com>
2019-03-10 20:33:18 +0100

    test buttons

--- code/code.ino
+++ code/code.ino
@@ -13,8 +13,8 @@ void setup() {
   PinFactory::initialize();
   PinFactory::runIntegratedCircuits();
 
-  StateFactory::initialize();
-  MachineFactory::initialize();
+  //StateFactory::initialize();
+  //MachineFactory::initialize();
 
   InterruptionRouter::enableInterruptions();
 
@@ -22,14 +22,18 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("OK");
+  //Serial.println("OK");
 
-  delay(50);
+  //delay(50);
 
-  ButtonHandler::handle(buttonPressed);
+  //ButtonHandler::handle(buttonPressed);
 
   // reset after handling
-  if(buttonPressed > 0) buttonPressed = 0;
+  if(buttonPressed > 0) {
+    Serial.println(buttonPressed);
+    
+    buttonPressed = 0;
+  }
 }
 
 // linux problems: sudo chmod a+rw /dev/ttyACM0
