346c4b94611b0436741c1bf5c649011010bd12c2
Mateusz Palichleb <mateusz1p@gmail.com>
2019-03-10 23:03:09 +0100

    lcd + midi

--- code/code.ino
+++ code/code.ino
@@ -4,24 +4,35 @@
 #include "InterruptionRouter.h"
 #include "ButtonHandler.h"
 
+ #include <MIDI.h>
+
+ // Created and binds the MIDI interface to the default hardware Serial port
+ MIDI_CREATE_DEFAULT_INSTANCE();
+
+
 volatile uint8_t buttonPressed = 0;
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
 
+  MIDI.begin(MIDI_CHANNEL_OMNI);  // Listen to all incoming messages
+
   PinFactory::initialize();
-  PinFactory::runIntegratedCircuits();
+  //PinFactory::runIntegratedCircuits();
 
-  //StateFactory::initialize();
-  //MachineFactory::initialize();
+  StateFactory::initialize();
+  MachineFactory::initialize();
 
-  InterruptionRouter::enableInterruptions();
+  //InterruptionRouter::enableInterruptions();
 
   Serial.println("Setup ready");
 }
 
 void loop() {
+  // Send note 42 with velocity 127 on channel 1
+     MIDI.sendNoteOn(42, 127, 1);
+
   //Serial.println("OK");
 
   //delay(50);
