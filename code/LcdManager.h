5747f0569723be9fc2acbc1ec6358bc1ab3fc18d
Mateusz Palichleb <mateusz1p@gmail.com>
2019-03-13 20:50:08 +0100

    LCD manager

---
+++ code/LcdManager.h
@@ -0,0 +1,19 @@
+#ifndef LCD_MANAGER_H
+#define LCD_MANAGER_H
+
+// https://github.com/duinoWitchery/hd44780
+
+#include <Wire.h>
+#include <hd44780.h>
+#include <hd44780ioClass/hd44780_I2Cexp.h>
+
+class LcdManager {
+private:
+    static hd44780_I2Cexp *lcd;
+public:
+    static void initialize();
+    static void printFirstRow();
+    static void printSecondRow();
+};
+
+#endif
--- code/code.ino
+++ code/code.ino
@@ -5,23 +5,9 @@
 #include "MachineFactory.h"
 #include "InterruptionRouter.h"
 #include "ButtonHandler.h"
+#include "LcdManager.h"
 
-#include <Wire.h>
-#include <hd44780.h>
-#include <hd44780ioClass/hd44780_I2Cexp.h>
-//https://github.com/FortySevenEffects/arduino_midi_library/blob/master/src/MIDI.h
-
- // Created and binds the MIDI interface to the default hardware Serial port
- //
-/*
- * Co brakuje:
- *
- * - jakie sygnały midi trzeba wyslac? i jaki typ?
- * - wyświetlanie czasów na ekranie i wciśniętych przycisków (stany) - czyli ekran musi być osobnym obiektem, aby tym sterował (blokował na sekundy)
- */
-
-//
-hd44780_I2Cexp lcd;
+//hd44780_I2Cexp lcd;
 volatile uint8_t buttonPressed = 0;
 
 void setup() {
@@ -37,28 +23,18 @@ void setup() {
   MachineFactory::initialize();
 
   KnobManager::initialize();
+  LcdManager::initialize();
 
   InterruptionRouter::enableInterruptions();
-
-  // initialize LCD with number of columns and rows:
-  lcd.begin(20, 4);
-
-  // Print a message to the LCD
-  lcd.print("OK");
 }
 
 void loop() {
-  // Send note 42 with velocity 127 on channel 1
-  // MIDI.sendNoteOn(42, 127, 1);
-
   KnobManager::updateAllKnobs();
+
   ButtonHandler::handle(buttonPressed);
 
   // reset after handling
   if(buttonPressed > 0) {
-    lcd.setCursor(0, 1);
-    lcd.print(buttonPressed);
-    
     buttonPressed = 0;
   }
 }
