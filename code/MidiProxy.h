5a529abe65d9b378d722e00d99f4d23cb80edf0e
Mateusz Palichleb <mateusz1p@gmail.com>
2019-03-16 23:25:00 +0100

    sync for knobs & rest knobs CC's

--- code/MidiProxy.h
+++ code/MidiProxy.h
@@ -7,10 +7,25 @@
 #define MIDI_DEFAULT_CHANNEL 10
 #define MIDI_MAX_VALUE 127
 
+// knobs
 #define CC_ECHO_1_TIME 22
 #define CC_ECHO_2_TIME 23
-#define CC_MODE 24
+#define CC_FEEDBACK 24
+#define CC_MIX 25
+#define CC_SATURATION 26
+#define CC_LOW_CUT 27
+#define CC_HIGH_CUT 28
+#define CC_INPUT 29
+#define CC_OUTPUT 30
+#define CC_FEEL 31
+#define CC_GROOVE 32
+#define CC_RHYTM_REPEATS 33
+#define CC_RHYTM_DECAY 34
 
+// steps / modes / styles
+#define CC_MODE 35
+
+// Notes (for buttons) on/off
 #define NOTE_C2 36
 
 class MidiProxy {
